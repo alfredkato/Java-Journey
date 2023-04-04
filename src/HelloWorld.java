3f1102ee36f57e766d49e0008a8d28d90425de3d
Alfred Kato <alfredchaze@gmail.com>
2023-04-04 12:45:59 -0700

    my first commit.

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("I just started my journey in programing");
+        System.out.println("and today is not an exception, i'm going at it again.");
+    }
+}
